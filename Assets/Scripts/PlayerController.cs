7e5b181fed810197ac203f69412a28f5d700297d
Gabriel Basilio Brito <gabrielbb0306@gmail.com>
2019-01-01 23:09:37 -0400

    Initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Boundary
+{
+    public float minX, maxX, minZ, maxZ;
+}
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rb;
+    public float speed;
+
+    public Boundary boundary;
+
+    public float tilt;
+
+    public GameObject shot;
+
+    public Transform shotSpawn;
+
+    public float fireRate;
+
+    private float nextShotTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("Fire1") && Time.time >= nextShotTime)
+        {
+            nextShotTime = Time.time + fireRate;
+            Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+        }
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        var movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rb.velocity = movement * speed;
+
+        rb.position = new Vector3(
+          Mathf.Clamp(rb.position.x, boundary.minX, boundary.maxX),
+          0.0f,
+          Mathf.Clamp(rb.position.z, boundary.minZ, boundary.maxZ)
+        );
+
+        rb.rotation = Quaternion.Euler(0.0f, 0.0f, rb.velocity.x * -tilt);
+    }
+}
---
+++ Assets/Scripts/RandomRotator.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomRotator : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public float rotationSpeed;
+
+    void Start()
+    {
+        GetComponent<Rigidbody>().angularVelocity = Random.onUnitSphere * rotationSpeed;
+    }
+}
