e75691a7c7701d663ab202a95f4ff748528bf18b
Gabriel Basilio Brito <gabrielbb0306@gmail.com>
2019-01-01 23:10:50 -0400

    Changing name of class because it conflicts with a Unity class

--- Assets/Scripts/RandomRotation.cs
+++ Assets/Scripts/RandomRotation.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RandomRotator : MonoBehaviour
+public class RandomRotation : MonoBehaviour
 {
     // Start is called before the first frame update
 
